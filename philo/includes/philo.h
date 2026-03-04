bbc1e161a69823911e6e42fae2964dd9834693fc
Adrien Fournier <adrien.fournier1998@hotmail.fr>
2026-03-04 11:55:42 +0100

    heure du miam

--- philo/includes/philo.h
+++ philo/includes/philo.h
@@ -6,37 +6,37 @@
 /*   By: afournie <afournie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/26 13:13:26 by afournie          #+#    #+#             */
-/*   Updated: 2026/03/04 11:33:36 by afournie         ###   ########.fr       */
+/*   Updated: 2026/03/04 11:42:09 by afournie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
 
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
 # include <pthread.h>
 # include <stdbool.h>
-# include <stdio.h>
-# include <stdlib.h>
 # include <sys/time.h>
-# include <unistd.h>
 
 typedef struct s_settings
 {
-	size_t			start_timestamp;
 	int				nb_philo;
-	int				time_to_die;
-	int				time_to_eat;
-	int				time_to_sleep;
 	int				nb_max_eat;
-	int				need_to_check_meal;
-	int				nb_philo_eaten_all;
 	int				philo_died;
+	int				time_to_eat;
+	int				time_to_die;
+	int				time_to_sleep;
 	bool			philo_eat_all;
 	bool			start_routine;
+	size_t			start_timestamp;
+	int				need_to_check_meal;
+	int				nb_philo_eaten_all;
 
 	pthread_mutex_t	nb_philo_eaten_all_mutex;
-	pthread_mutex_t	philo_died_mutex;
 	pthread_mutex_t	philo_eat_all_mutex;
+	pthread_mutex_t	philo_died_mutex;
 	pthread_mutex_t	print_mutex;
 
 }					t_settings;
@@ -49,9 +49,9 @@ typedef struct s_fork
 typedef struct s_philo
 {
 	int				id;
+	pthread_t		thread;
 	int				nb_eaten;
 	long			last_meal;
-	pthread_t		thread;
 	t_settings		*settings;
 	t_fork			*left_fork;
 	t_fork			*right_fork;
@@ -59,6 +59,7 @@ typedef struct s_philo
 	pthread_mutex_t	meal_mutex;
 }					t_philo;
 
+int					check_args(int ac);
 void				eat(t_philo *philo);
 void				think(t_philo *philo);
 void				sleepy(t_philo *philo);
@@ -66,7 +67,6 @@ size_t				get_current_time(void);
 int					ft_atoi(const char *str);
 void				take_fork(t_philo *philo);
 void				release_fork(t_philo *philo);
-int					check_args(int ac);
 bool				start_routine(t_philo *philo);
 void				mutex_init(t_settings *settings);
 void				free_settings(t_settings *settings);
